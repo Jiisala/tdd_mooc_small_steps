8911a5664667aecf12334f6c053690a4a6d17772
Jaakko <jiisala@gmail.com>
2022-12-29 18:52:38 +0200

    second refactor

--- src/prices.mjs
+++ src/prices.mjs
@@ -20,7 +20,7 @@ function createApp(database) {
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
     const date = parseDate(req.query.date);
-    const cost = calculateCost(age, type, convert(date), baseCost);
+    const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
 
@@ -33,7 +33,7 @@ function createApp(database) {
   }
   function parseDate(dateString) {
     if (dateString) {
-      return new Date(dateString);
+      return Temporal.PlainDate.from(dateString)
     }
   }
   
